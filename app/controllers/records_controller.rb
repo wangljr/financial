9d4467b373ad526bc78ee75059106bf4568057aa
Robert Wang <wangljr@163.com>
2011-12-26 15:17:40 +0800

    添加私有方法，计算总数

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,11 +4,17 @@ class RecordsController < ApplicationController
   def index
     @month = Time.now.strftime("%Y-%m")
     @records = Record.find_by_month(@month).paginate(:page => params[:page], :per_page => 15)
+    records = Record.find_by_month(@month)
+    @income_sum = income_sum records
+    @outlay_sum = outlay_sum records
   end
 
   def month
     @month = params[:month] || Time.now.strftime("%Y-%m")
     @records = Record.find_by_month(@month).paginate(:page => params[:page], :per_page => 15)
+    records = Record.find_by_month(@month)
+    @income_sum = income_sum records
+    @outlay_sum = outlay_sum records
   end
 
   def category
@@ -54,5 +60,45 @@ class RecordsController < ApplicationController
     @record = Record.find(params[:id])
     @record.destroy
     redirect_to(records_path)
+  end
+
+  private
+  def income_sum records
+    sum = 0
+    unless records.nil? || records.empty?
+      records.each do |record|
+        sum += income record unless income(record).nil?
+      end
+    end
+    return sum
+  end
+
+  private
+  def outlay_sum records
+    sum = 0
+    unless records.nil? || records.empty?
+      records.each do |record|
+        sum += outlay record unless outlay(record).nil?
+      end
+    end
+    return sum
+  end
+
+  private
+  def income record
+    cate = record.category
+    until cate.category.nil?
+      cate = cate.category
+    end
+    return cate.id==1 ? record.fee : nil
+  end
+
+  private
+  def outlay record
+    cate = record.category
+    until cate.category.nil?
+      cate = cate.category
+    end
+    return cate.id==2 ? record.fee : nil
   end
 end
