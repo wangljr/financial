75e5c9e21bd6f8fd078eee45d544a70a1d05bc8b
Robert Wang <wangljr@163.com>
2012-01-09 11:06:12 +0800

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,14 +11,14 @@ class HomeController < ApplicationController
     @sums = []
     Category.outlay.first.categories.each do |category|
       sum = 0
-      records = category.records
+      records = category.records.find_by_month Time.now.strftime("%Y-%m")
       unless records.empty?
         records.each do |record|
           sum += record.fee
         end
       end
       category.categories.each do |sub_cate|
-        records = sub_cate.records
+        records = sub_cate.records.find_by_month Time.now.strftime("%Y-%m")
         unless records.empty?
           records.each do |record|
             sum += record.fee
